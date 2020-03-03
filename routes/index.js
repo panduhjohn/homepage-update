f6823468b36bd109545589caad028967ea760ec0
John Stilwell <johnrstilwell@codeimmersives.com>
2020-03-03 16:35:21 -0500

    moved products from home to loader

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const items = require('../lib/loader');
+
 //Render homepage
 router.get('/', (req, res, next) => {
-  return res.render('main/home');
+    return res.render('main/home', { items });
 });
 
 router.get('/logout', (req, res) => {
-    req.logout()
-    req.session.destroy()
-    return res.redirect('/')
-})
+    req.logout();
+    req.session.destroy();
+    return res.redirect('/');
+});
 
 module.exports = router;
